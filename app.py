207a6d6c516d0608ed2e3dc3e87591a393ecdbf3
PropeReferio <lemuel.b.stevens@gmail.com>
2020-12-02 21:10:57 -0600

    string edit

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-	return 'Hey EQFX team, we have Flask in a Docker container and GKE!'
+	return 'Hey team, we have Flask in a Docker container and GKE!'
 
 if __name__ == '__main__':
 	app.run(debug=True, host='0.0.0.0')
